279c0e3cf89634af37215f82d2a1f49d2aa7b7ad
tsekenrick <kt1556@nyu.edu>
2019-02-14 17:19:14 -0500

    first actual commit! beginnings of constructing architecture, please look forward to it

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+
+    public static Board me;
+    public GameObject player;
+    public GameObject[] enemies;
+
+    GameObject enemy;
+    enum Phase {Mulligan, Play, Resolution};
+    Phase curPhase;
+
+    public GameObject cardPrefab;
+    private List<GameObject> pool = new List<GameObject>();
+    public Dictionary<string, Vector3> cardAnchors = new Dictionary<string, Vector3>();
+
+    public List<Card> deck = new List<Card>();
+    public List<Card> discard = new List<Card>();
+    public List<Card> hand = new List<Card>();
+    public int turn;
+
+
+    void Awake(){
+        me=this;
+    }
+
+    void Start(){
+        Phase curPhase = Phase.Mulligan;
+
+        // get anchor positions
+        cardAnchors.Add("Deck Anchor", GameObject.Find("_DeckAnchor").transform.position);
+        for(int i = 0; i < 5; i++){
+            cardAnchors.Add($"Hand {i}", GameObject.Find($"Hand{i}").transform.position);
+        }
+        cardAnchors.Add("Discard Anchor", GameObject.Find("_DiscardAnchor").transform.position);
+
+        for(int i = 0; i < 20; i++){
+            GameObject curCard = Instantiate(cardPrefab, cardAnchors["Deck Anchor"], Quaternion.identity);
+            pool.Add(curCard);
+            curCard.SetActive(false);
+        }
+
+        // TODO: draw cards lol
+    }
+    void Update(){
+        switch(curPhase){
+            case Phase.Mulligan:
+                break;
+            case Phase.Play:
+                break;
+            case Phase.Resolution:
+                break;
+        }
+            
+    }
+}
---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Card : MonoBehaviour
+{
+    enum CardState {inDeck, inHand, inDiscard}; 
+    public GameObject card;
+    private Board board = Board.me;
+
+    void attack(int amount, Target target){
+
+    }
+
+    void defend(int amount, Target target){
+
+    }
+
+    void Start(){
+        card = GetComponent<GameObject>();
+        CardState cardState = CardState.inDeck;
+        card.transform.DOMove(new Vector3(3,3,3),1);
+    }
+    
+}
