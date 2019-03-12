8e29f2fed48ccc2a0ce679d9fa03a3c248c4bc12
tsekenrick <kt1556@nyu.edu>
2019-03-12 17:18:15 -0400

    progress bar on timeline now fills relative to length of playSequence

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ProgressBar : MonoBehaviour
+{
+    private Board board;
+
+    void Start() {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, .5f);
+        board = Board.me;
+    }
+
+    void Update() {
+        if(board.curPhase == Phase.Play || board.curPhase == Phase.Resolution) {
+            transform.DOScaleX(board.playSequence.totalTime, .2f * board.playSequence.totalTime);
+        }
+    }
+}
