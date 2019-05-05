bab816bf98a795c3dfb3e4fb7059b8a518061597
tsekenrick <kt1556@nyu.edu>
2019-05-05 16:48:22 -0400

    prefabs and scripts for our 4 events are in - next up is putting them on screen on enemy death

--- Assets/Scripts/Event.cs
+++ Assets/Scripts/Event.cs
@@ -2,17 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Event : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Event : MonoBehaviour {
+    
+    public Sprite[] eventStates;
+    private SpriteRenderer sr;
+    private Board board;
+
+    // do the thing the event says it will do, and then set game back to mul phase
+    protected virtual void resolveEvent() {
+
+        // reset combat states
+        Board.me.curPhase = Phase.Mulligan;
+        board.mulLimit = 4;
+        board.turn = 0;
+        board.borrowedTime = 0;
+        board.round = 0;
+        board.Reshuffle();
+        return;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    protected virtual void Start() {
+        sr = this.GetComponent<SpriteRenderer>();
+        board = Board.me;
+    }
+
+    void OnMouseEnter() {
+        sr.sprite = eventStates[1];
+    }
+
+    void OnMouseExit() {
+        sr.sprite = eventStates[0];
+    }
+
+    void OnMouseUpAsButton() {
+        resolveEvent();
     }
 }
