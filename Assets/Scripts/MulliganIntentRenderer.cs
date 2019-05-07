ee0ea9fb66f59d0ab8f8492189f2d206782a8ba2
tsekenrick <kt1556@nyu.edu>
2019-05-07 18:09:52 -0400

    new mulligan intent icons with bounce

--- Assets/Scripts/MulliganIntentRenderer.cs
+++ Assets/Scripts/MulliganIntentRenderer.cs
@@ -1,12 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class MulliganIntentRenderer : MonoBehaviour {
-    
+
     public Sprite[] mulliganIntentIcons;
+    private SpriteRenderer sr;
+
+
+    private int DetermineActionType(List<EnemyAction> actions) {
+        bool hitAttack = false, hitDefend = false;
+        foreach(EnemyAction action in actions) {
+            if(action.actionType == ActionType.Attack && !hitAttack) {
+                hitAttack = true;
+            } else if(action.actionType == ActionType.Defense && !hitDefend) {
+                hitDefend = true;
+            }
+        }
+        if(hitAttack && hitDefend) {
+            return 2;
+        } else if (hitDefend) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    void Start() {
+        sr = GetComponent<SpriteRenderer>();
+        transform.DOMoveY(transform.position.y + .2f, .7f).SetLoops(-1, LoopType.Yoyo);
+    }
 
     void Update() {
-        
+        if(Board.me.curPhase == Phase.Mulligan) {
+            sr.enabled = true;
+            sr.sprite = mulliganIntentIcons[DetermineActionType(transform.parent.GetComponent<Enemy>().curActions)];
+        } else {
+            sr.enabled = false;
+        }
     }
 }
