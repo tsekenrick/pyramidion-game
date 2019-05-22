a2e45f92c038b7aa8bc2522ff2b7d86e8c32ea94
tsekenrick <kt1556@nyu.edu>
2019-05-22 01:16:47 -0400

    made menu buttons uninteractive when reading instructions

--- Assets/Scripts/Menu/InstructionsMenuButton.cs
+++ Assets/Scripts/Menu/InstructionsMenuButton.cs
@@ -16,10 +16,25 @@ public class InstructionsMenuButton : MenuButton {
     public GameObject returnButton;
 
     protected override void OnMouseUpAsButton() {
+        if(GameObject.Find("_DarknessOverlay").GetComponent<SpriteRenderer>().enabled) return;
         base.OnMouseUpAsButton();
         isDisplaying = true;
     }
 
+    protected override void OnMouseExit() {
+        if(GameObject.Find("_DarknessOverlay").GetComponent<SpriteRenderer>().enabled) return;
+        base.OnMouseExit();
+    }
+
+    protected override void OnMouseEnter() {
+        if(GameObject.Find("_DarknessOverlay").GetComponent<SpriteRenderer>().enabled) return;
+        base.OnMouseEnter();
+    }
+
+    protected override void OnMouseDown() {
+        if(GameObject.Find("_DarknessOverlay").GetComponent<SpriteRenderer>().enabled) return;
+        base.OnMouseDown();
+    }
     protected void Start() {
         overlay = GameObject.Find("_DarknessOverlay").GetComponent<SpriteRenderer>();
         slideContainer = transform.Find("CurrentSlide").GetComponent<SpriteRenderer>();
--- Assets/Scripts/Menu/PlayMenuButton.cs
+++ Assets/Scripts/Menu/PlayMenuButton.cs
@@ -7,7 +7,23 @@ public class PlayMenuButton : MenuButton {
     [SerializeField]
     private int sceneToLoad;
     protected override void OnMouseUpAsButton() {
+        if(GameObject.Find("_DarknessOverlay").GetComponent<SpriteRenderer>().enabled) return;
         base.OnMouseUpAsButton();
         SceneManager.LoadScene(sceneToLoad);
+    }
+
+    protected override void OnMouseExit() {
+        if(GameObject.Find("_DarknessOverlay").GetComponent<SpriteRenderer>().enabled) return;
+        base.OnMouseExit();
+    }
+
+    protected override void OnMouseEnter() {
+        if(GameObject.Find("_DarknessOverlay").GetComponent<SpriteRenderer>().enabled) return;
+        base.OnMouseEnter();
+    }
+
+    protected override void OnMouseDown() {
+        if(GameObject.Find("_DarknessOverlay").GetComponent<SpriteRenderer>().enabled) return;
+        base.OnMouseDown();
     }
 }
